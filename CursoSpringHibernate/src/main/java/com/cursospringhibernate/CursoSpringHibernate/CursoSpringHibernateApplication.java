75d047359100bb95167ab3f9d125ded6feaf347a
eujoaolima <joaopedro.donatolima@hotmail.com>
2023-08-28 15:52:34 -0300

    first commit ;)

---
+++ CursoSpringHibernate/src/main/java/com/cursospringhibernate/CursoSpringHibernate/CursoSpringHibernateApplication.java
@@ -0,0 +1,14 @@
+package com.cursospringhibernate.CursoSpringHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringHibernateApplication.class, args);
+		System.out.println("Server running!");
+	}
+
+}
