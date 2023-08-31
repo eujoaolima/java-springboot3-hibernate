791b163c082b786bdb3dec40826167085b6d6e3b
eujoaolima <joaopedro.donatolima@hotmail.com>
2023-08-31 16:30:38 -0300

    spring-boot-rest-security, spring-boot-spring-mvc, spring-boot-spring-mvc-crud

---
+++ 05 - spring-boot-rest-security/00 - spring-boot-rest-security-employee-starter-code/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+w
+Gsrc/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java,1\e\1e25ba54f8ad0a1f735be8c92ad95b7dc6dcdd3b
+
+Osrc/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java,1\c\1c1c3969c7744205910a636684abeb6203ccf9e6
---
+++ 06 - spring-boot-spring-mvc/02 - validation-demo/src/main/java/com/spring/thymeleaf/demo/mvc/ValidationdemoApplication.java
@@ -0,0 +1,14 @@
+package com.spring.thymeleaf.demo.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationdemoApplication.class, args);
+		System.out.println("Server running :)");
+	}
+
+}
